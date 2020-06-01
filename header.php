46343ebe2ed203c52dbbaf0197bf208995c9acf9
Tim <github@timgreen.ws>
2020-06-01 19:39:46 +1000

    added viewport meta into theme

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <?php
