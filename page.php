bf875c126e7da522f8111e91d5285e1b6f226592
Tim <github@timgreen.ws>
2020-06-01 21:38:50 +1000

    default page to include content and caption-non from partials

--- page.php
+++ page.php
@@ -1,43 +1,26 @@
 <?php
 /**
- * Template Name: Page (Default)
- * Description: Page template with Sidebar on the left side
- *
+ * Template Name: Page post template
+ * @package captionwp
+ * @since 1.1
  */
 
 	get_header();
 ?>
 
-	<div class="row">
-		
-		<div class="col-md-8 order-md-2 col-sm-12">
-			<?php the_post(); ?>
-			
-			<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				
-				<?php
-					the_content();
-					
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . __( 'Pages:', 'my-theme' ),
-						'after'  => '</div>',
-					) );
-					edit_post_link( __( 'Edit', 'my-theme' ), '<span class="edit-link">', '</span>' );
-				?>
-			</div><!-- /#post-<?php the_ID(); ?> -->
-			
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-			
-		</div><!-- /.col -->
-		
-		<?php get_sidebar(); ?>
-		
-	</div><!-- /.row -->
+	<section class="content" id="content">
+	<?php
+        if ( have_posts() ) {
+            while( have_posts() ) {
+                the_post();
+
+                // Get page content partial
+                get_template_part( 'partials/content', 'page' );
+            }
+         } else {
+            get_template_part( 'partials/caption', 'none' );
+         }
+    ?>
+	</section>
 
 <?php get_footer(); ?>
