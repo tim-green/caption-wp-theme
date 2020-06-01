9ee182ddb85ffa18d531391f8019f6aaa9a877e8
Tim <github@timgreen.ws>
2020-06-01 21:33:51 +1000

    Created 404 page to show if there isn't a image to show

--- 404.php
+++ 404.php
@@ -2,18 +2,15 @@
 /**
  * Template Name: Not found
  * Description: Page template 404 Not found
- *
+ * @package captionwp
+ * @since 1.1
  */
 
 	get_header();
 ?>
 
-	<div id="post-0" class="content error404 not-found">
-		<h1 class="entry-title"><?php _e( 'Not found', 'my-theme' ); ?></h1>
-		<div class="entry-content">
-			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'my-theme' ); ?></p>
-			<div><?php get_search_form(); ?></div>
-		</div><!-- /.entry-content -->
-	</div><!-- /#post-0 -->
+<section class="content" id="content">
+	<?php get_template_part('partials/caption','none');?>
+</section>
 
 <?php get_footer(); ?>
