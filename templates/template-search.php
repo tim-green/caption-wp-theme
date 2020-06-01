b4a47e7e0699c08e183268be93b34ebe42c388eb
Tim <github@timgreen.ws>
2020-06-01 21:40:56 +1000

    template to get default wp search form

---
+++ templates/template-search.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Part: Search page
+ * @package captionwp
+ * @since 1.1
+ */
+get_header(); ?>
+
+<section class="content">
+    <?php get_search_form(); ?>
+</section>
+
+<?php get_footer();
