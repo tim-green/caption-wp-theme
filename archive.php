714b6dbe2ac670f215469a29ab1581fb72b2bc6e
Tim <github@timgreen.ws>
2020-06-01 21:34:06 +1000

    created archive for images & captions

--- archive.php
+++ archive.php
@@ -1,41 +1,33 @@
 <?php
 /**
  * The Template for displaying Archive pages.
+ * @package captionwp
+ * @since 1.1
  */
 
 	get_header();
 ?>
 
-	<?php if ( have_posts() ) : ?>
-
-		<header class="page-header">
-			<h1 class="page-title">
-				<?php if ( is_day() ) : ?>
-					<?php printf( __( 'Daily Archives: %s', 'my-theme' ), '<span>' . get_the_date() . '</span>' ); ?>
-				<?php elseif ( is_month() ) : ?>
-					<?php printf( __( 'Monthly Archives: %s', 'my-theme' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'my-theme' ) ) . '</span>' ); ?>
-				<?php elseif ( is_year() ) : ?>
-					<?php printf( __( 'Yearly Archives: %s', 'my-theme' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'my-theme' ) ) . '</span>' ); ?>
-				<?php else : ?>
-					<?php _e( 'Blog Archives', 'my-theme' ); ?>
-				<?php endif; ?>
-			</h1>
-		</header>
-
-		<?php
-			get_template_part( 'archive', 'loop' );
-		?>
-
-	<?php else : ?>
-
-		<?php
-			// 404
-			get_template_part( 'content', 'none' );
-		?>
-
+	<section class="content" id="content">
 	<?php
-		endif;
-		wp_reset_postdata(); // end of the loop.
-	?>
+        if ( have_posts() ) {
+            if ( get_the_archive_title() ) {
+                get_template_part( 'partials/caption' );
+            }
 
+            while( have_posts() ) {
+                the_post();
+
+                // Include default content partial
+                get_template_part( 'partials/content' );
+            }
+
+            // Show navigation
+            the_posts_pagination();
+        } else {
+            // If no content, mention the "No posts found" template
+            get_template_part( 'partials/caption', 'none' );
+        }
+    ?>
+	</section>
 <?php get_footer(); ?>
