a9740049d9244a3d4fa86195593f1c97ef69343b
Tim <github@timgreen.ws>
2025-09-15 10:45:32 +1000

    updated project from gruntjs to vite

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// assets/js/app.js
+
+/**
+ * 
+ * Import the main css file
+ * Import all the JavaScript files that are needed for the theme.
+ */
+
+// Import the main CSS file
+import '../sass/app.scss'
+
+// Build the main JS file
+// ... other js files imports
